05310a4164bba6e5d44ed8135d5f6c313dc1e71c
mislavkostic <mkostic@etfos.hr>
2022-04-26 14:32:42 +0200

    added profile page and chat window

--- js/chatroom.js
+++ js/chatroom.js
@@ -1,11 +1,20 @@
+//TODO: refactor code duplicates
+
 let htmlBody = document.querySelector('body')
+
+let chatTitle = document.createElement('div')
+chatTitle.innerText = "Bubbles"
+chatTitle.className = "chat-title"
+
 let chatWindow = document.createElement('div')
 chatWindow.className= "chat-window"
-chatWindow.innerText = "Bubbles"
 
 let messageBox= document.createElement('div')
 messageBox.className = "message-box"
 chatWindow.appendChild(messageBox)
+
+let inputBox = document.createElement('div')
+inputBox.className = "input-box"
 
 let inputMessage = document.createElement('input')
 inputMessage.type = "text"
@@ -16,17 +25,20 @@ let buttonSendMessage = document.createElement('button')
 buttonSendMessage.innerText = "Send"
 buttonSendMessage.addEventListener('click',() =>{
     addNewMessage()
+    chatWindow.scrollTop = chatWindow.scrollHeight;
 })
 
-
+htmlBody.appendChild(chatTitle)
 htmlBody.appendChild(chatWindow)
-htmlBody.appendChild(inputMessage)
-htmlBody.appendChild(buttonSendMessage)
+inputBox.appendChild(inputMessage)
+inputBox.appendChild(buttonSendMessage)
+htmlBody.appendChild(inputBox)
 
 document.addEventListener('keypress',(e) =>{
     if(e.key === "Enter"){
         if(e.target.className == "message-input"){
             addNewMessage()
+            chatWindow.scrollTop = chatWindow.scrollHeight;
         }
     }
  
@@ -37,7 +49,7 @@ function addNewMessage(){
     let message = document.createElement('p')
     message.className = "message-p"
     message.innerText = inputMessage.value
-    if(message.innerText == null || message.innerText == ""){
+    if(!message.innerText){
         return
     }
     messageDiv.appendChild(message)
